c600cfd28c69a261cf9c81aaa824a48dc443515f
Adama <samake8@gmail.com>
2021-03-22 09:02:14 +0100

    wip

--- package/ml_api/api/helper.py
+++
@@ -1,5 +0,0 @@
-allowedExtentions = ['csv']
-
-def allowed_file(*, file: str) -> bool:
-  extension = file.rsplit('.', 1)[1] 
-  return extension in allowedExtentions
--- package/ml_api/tests/test_controller.py
+++
@@ -1,5 +0,0 @@
-from wafer_fault_detection.predict import make_prediction
-
-def test_health_endpoint_returns_200(flask_test_client):
-  response = flask_test_client.get('/health')
-  assert response.status_code == 200
